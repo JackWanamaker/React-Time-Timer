c6f6832c467d9b28a95466c4b4565caff53681e7
Ben Summer <37679388+JackWanamaker@users.noreply.github.com>
2025-09-14 00:34:10 -0400

    Made the start, stop, pause, and resume buttons work correctly

--- src/App.jsx
+++ src/App.jsx
@@ -6,67 +6,74 @@ import Arc from './Arc'
 
 function App() {
   //Template variable
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
   const [arcAngle, setArcAngle] = useState(359.99);
   const [timerLength, setTimerLength] = useState(1);
   const refreshTime = useRef(5);
   const [timeLeft, setTimeLeft] = useState(Infinity);
   const [timeElapsed, setTimeElapsed] = useState(0);
   const startTime = useRef(Date.now());
+  const [cachedTime, setCachedTime] = useState(0);
   const [isRunning, setIsRunning] = useState(false);
 
   function handleChange(e) {
-    setTimerLength(e.target.value)
+    setTimerLength(e.target.value);
   }
 
   function startTimer() {
-    setIsRunning(true);
     startTime.current = Date.now();
+    setTimeLeft(Infinity);
+    setArcAngle(359.99);
+    setCachedTime(0);
+    setIsRunning(true);
     console.log("Started");
     console.log("Timer Length Currently " + timerLength);
   }
 
   function resumeTimer() {
+    startTime.current = Date.now();
     setIsRunning(true);
-    startTime.current = Date.now()
     console.log("Resumed");
     console.log("Time Left: " + timeLeft);
+    console.log("Timer Length Currently " + timerLength);
+    console.log("Cached Time: " + cachedTime);
   }
 
   function pauseTimer() {
     setIsRunning(false);
-    setTimerLength(timerLength)
-    console.log("Paused")
+    setCachedTime(timerLength*1000 - timeLeft);
+    console.log("Paused");
   }
 
   function stopTimer() {
     setIsRunning(false);
     setTimeLeft(Infinity);
     setArcAngle(359.99);
+    setCachedTime(0);
     console.log("Stopped");
   }
 
   useEffect(() => {
     if (isRunning) {
       if (timeLeft <= 0) {
-        console.log("I'm here")
+        console.log("I'm here");
         stopTimer();
         return; // stop when 0
       }
       
       const timer = setInterval(() => {
-      let currentTime = Date.now()
-      //console.log("Start Time: " + startTime.current)
-      //console.log("Current Time: " + currentTime)
-      //console.log(currentTime-startTime.current)
-      setTimeElapsed(timeElapsed + (currentTime));
-      setTimeLeft(timerLength*1000 - (currentTime - startTime.current));
-      //console.log(timeLeft)
+      let currentTime = Date.now();
+      //console.log("Start Time: " + startTime.current);
+      //console.log("Current Time: " + currentTime);
+      //console.log(currentTime-startTime.current);
+      //setTimeElapsed(timeElapsed + (currentTime));
+      setTimeLeft(((timerLength*1000) - cachedTime) - (currentTime - startTime.current));
+      //console.log(timeLeft);
       if (timeLeft == timerLength*1000){
-        setArcAngle(359.99)
+        setArcAngle(359.99);
       }
       else {
-        setArcAngle((timeLeft/(timerLength*1000))*360)
+        setArcAngle((timeLeft/(timerLength*1000))*360);
       }
     }, refreshTime.current);
     return () => clearInterval(timer); // cleanup when unmounted
