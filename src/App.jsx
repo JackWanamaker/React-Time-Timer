c6e32a9063c16c01b721fff32b62b650efd672b7
Ben Summer <37679388+JackWanamaker@users.noreply.github.com>
2025-09-16 15:45:48 -0400

    Removed template variables

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,10 @@ import './App.css'
 import Arc from './Arc'
 
 function App() {
-  //Template variable
-  const [count, setCount] = useState(0);
   const [arcAngle, setArcAngle] = useState(359.99);
   const [timerLength, setTimerLength] = useState(1);
   const refreshTime = useRef(5);
   const [timeLeft, setTimeLeft] = useState(Infinity);
-  const [timeElapsed, setTimeElapsed] = useState(0);
   const startTime = useRef(Date.now());
   const [cachedTime, setCachedTime] = useState(0);
   const [isRunning, setIsRunning] = useState(false);
@@ -84,23 +81,6 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
       <Arc radius={80} startAngle={0} endAngle={arcAngle} stroke="black" strokeWidth={1.5} />
       <button onClick={startTimer}>Start Timer </button>
       <button onClick={resumeTimer}>Resume Timer </button>
@@ -108,9 +88,6 @@ function App() {
       <button onClick={stopTimer}>Stop Timer</button>
       <label>Time (seconds):</label>
         <input type="number" value={timerLength} onChange={handleChange} min="1" max="60"/>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
